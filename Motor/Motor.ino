a8f34a7de9d48b5bb16e10d3525c724310a91adf
GeorgeOfGithub <54135275+GeorgeOfGithub@users.noreply.github.com>
2019-09-16 14:30:51 +0200

    Update Motor.ino

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -1,45 +1,41 @@
-const int motor1A = 13;
-const int motor1B = 12;
-const int motor2A = 11;
-const int motor2B = 10;
+const int motor1A = 10;
+const int motor1B = 11;
 const int sensor = 9;
+const int rod = 0;
 
 void setup() {
   pinMode(motor1A, OUTPUT);
   pinMode(motor1B, OUTPUT);
-  pinMode(motor2A, OUTPUT);
-  pinMode(motor2B, OUTPUT);
-  pinMode(sensor,INPUT);
+ 
+  
 }
 
 void loop() {
   frem();
   delay(3000);
-  drejHoejre();
-  delay(2000);
   stop();
-  delay(1000);
+  delay(2000);
 
 }
 
 void frem() {
-  digitalWrite(motor1A,LOW);
+  digitalWrite(motor1A,HIGH);
   digitalWrite(motor1B,HIGH);
-  digitalWrite(motor2A,LOW);
-  digitalWrite(motor2B,HIGH);
+  analogWrite(motor1A,200);
+  analogWrite(motor1B,100);
   
 }
 
 void drejHoejre() {
-  digitalWrite(motor1A,LOW);
+  digitalWrite(motor1A,HIGH);
   digitalWrite(motor1B,HIGH);
-  digitalWrite(motor2B,LOW);
-  digitalWrite(motor2A,HIGH);
+  analogWrite(motor1A,75);
+  analogWrite(motor1B,200);
 }
 
 void stop() {
   digitalWrite(motor1A,LOW);
   digitalWrite(motor1B,LOW);
-  digitalWrite(motor2A,LOW);
-  digitalWrite(motor2B,LOW);
+  analogWrite(motor1A,0);
+  analogWrite(motor1B,0);
 }
