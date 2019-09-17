ae6c23fb9a56d46c12b73cf53d7706fa9d87eb5b
GeorgeOfGithub <54135275+GeorgeOfGithub@users.noreply.github.com>
2019-09-17 08:31:10 +0200

    Update Motor.ino

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -21,7 +21,7 @@ void loop() {
 void frem() {
   digitalWrite(motor1A,HIGH);
   digitalWrite(motor1B,HIGH);
-  analogWrite(motor1A,200);
+  analogWrite(motor1A,158);
   analogWrite(motor1B,100);
   
 }
