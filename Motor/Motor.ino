72b8707666130285bcaf0e520715e3d17b3bfbc5
GeorgeOfGithub <54135275+GeorgeOfGithub@users.noreply.github.com>
2019-09-17 12:56:18 +0200

    Update Motor.ino

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -21,16 +21,50 @@ void loop() {
 void frem() {
   digitalWrite(motor1A,HIGH);
   digitalWrite(motor1B,HIGH);
+<<<<<<< Updated upstream
   analogWrite(motor1A,158);
   analogWrite(motor1B,100);
   
 }
 
 void drejHoejre() {
+=======
+  digitalWrite(motor2B,LOW);
+  analogWrite(motor1A,158);
+  analogWrite(motor1B,100);
+  
+}
+void back() {
+  digitalWrite(motor1A,LOW);
+  digitalWrite(motor2A,HIGH);
+  digitalWrite(motor1B,LOW);
+  digitalWrite(motor2B,HIGH);
+  analogWrite(motor2A,158);
+  analogWrite(motor2B,100);
+  
+}
+
+void right() {
+  digitalWrite(motor1A,LOW);
+  digitalWrite(motor2A,HIGH);
+  digitalWrite(motor1B,HIGH);
+  digitalWrite(motor2B,LOW);
+  analogWrite(motor2A,208);
+  analogWrite(motor2B,100);
+}
+
+void left() {
+>>>>>>> Stashed changes
   digitalWrite(motor1A,HIGH);
   digitalWrite(motor1B,HIGH);
+<<<<<<< Updated upstream
   analogWrite(motor1A,75);
   analogWrite(motor1B,200);
+=======
+  digitalWrite(motor2B,LOW);
+  analogWrite(motor2A,100);
+  analogWrite(motor2B,208);
+>>>>>>> Stashed changes
 }
 
 void stop() {
