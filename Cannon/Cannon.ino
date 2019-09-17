029fcb4411b9b611976bbded335d8963833f2a88
ehmbie <markusbrodersen99@gmail.com>
2019-09-17 15:03:07 +0200

    Update Cannon.ino

--- Cannon/Cannon.ino
+++ Cannon/Cannon.ino
@@ -1,38 +1,40 @@
 #include <Servo.h>
-
 Servo cannonservo;
-
 int angle = 0;
-
 const int in1Pin = 5;
 const int in2Pin = 4;
 
 void setup() {
   cannonservo.attach(9);
-  Serial.begin(9600);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
-  Serial.println("Det kører");
 }
+
+void MedUret() {
+  digitalWrite(in1Pin,HIGH);
+  digitalWrite(in2Pin,LOW);
+}
+
+void ModUret() {
+  digitalWrite(in1Pin,LOW);
+  digitalWrite(in2Pin,HIGH);
+}
+
+void Stop() {
+  digitalWrite(in1Pin,LOW);
+  digitalWrite(in2Pin,LOW);
+}
+
 
 void loop() {
   cannonservo.write(angle);
-  if (Serial.available()) {
-    char ch = Serial.read();
-    if (ch == '+')  {
-      Serial.println("Bip");
-      digitalWrite(in1Pin,LOW);
-      digitalWrite(in2Pin,HIGH);
-    }
-    else if (ch == '-') {
-      Serial.println("Bap");
-      digitalWrite(in1Pin,HIGH);
-      digitalWrite(in2Pin,LOW);
-    }
-    else  {
-      Serial.println("Stop");
-      digitalWrite(in1Pin,LOW);
-      digitalWrite(in2Pin,LOW);
-    }
+  /*if (x = true && y = false && z = false) {
+    MedUret(); 
   }
+  else if (x = false && y = true && z = false)  {
+    ModUret();
+  }
+  else if (x = false && y = false && z = true)  {
+    Stop();
+  }*/
 }
